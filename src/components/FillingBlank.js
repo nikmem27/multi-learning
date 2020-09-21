fcfd8622d9a5e47ea0497a9da5b82fd3b59ed8c0
Nikos Memos <memnik@hotmail.com>
2020-09-21 10:02:10 +0300

    add naswers on state

--- src/components/FillingBlank.js
+++ src/components/FillingBlank.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { Component, useEffect } from 'react';
 import { useParams, withRouter } from 'react-router-dom';
 import { Input, Button } from 'reactstrap';
 
@@ -15,9 +15,57 @@ function randomOrder(array) {
     return array;
 }
 
+class Question extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        for (let key in this.state) {
+            let value = this.state[key]
+            if (key != value) {
+                console.log(key, value);
+                console.log(`You have problem with number ${key / this.props.match.params.number}`);
+            }
+
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    elements = randomOrder(elements)
+
+    render() {
+        let number = this.props.match.params.number;
+        
+        return (
+            <div>
+                <h1>Test for Number {number}</h1>
+                <ul>
+                    {elements.map((value, index) => {
+                        let multi = `${number} * ${value}`;
+                        let correctAnswer = number * value;
+                        return <p key={index}>{multi} =
+                    <Input type="number" name={`${correctAnswer}`} placeholder=" Your answer" onChange={this.handleChange} />
+                        </p>
+                    })}
+                </ul>
+                <Button onClick={this.handleSubmit}>Submit</Button>
+            </div>
+        )
+    }
+}
 
 //elements = randomOrder(elements);
-
+/*
 function Question() {
     let { number } = useParams();
     
@@ -26,6 +74,10 @@ function Question() {
     }, [number])    //we made this in order to refresh the random list only when our paremeter number is changed. Because if we don't do that, the random ...
                     //...list is refreshed when we hit the toggle button too. When we hit the toggle button the number doesn't change, so the random list will not be refreshed
     //console.log('skata'); Remeber to check that this logs triggers many times just on a refresh
+
+    function handleSubmit(event) {
+        console.log('skata');
+    }
 
     return (
         <div>
@@ -37,9 +89,10 @@ function Question() {
                     </p>
                 })}
             </ul>
-            <Button>Submit</Button>
+            <Button onClick={handleSubmit}>Submit</Button>
         </div>
     )
 }
+*/
 
 export default Question;
